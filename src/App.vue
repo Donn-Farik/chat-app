366c14d74576677155c67391e7ad677eb6249c89
Fakhreddine Farik <donnmoreno400@gmail.com>
2022-10-25 22:52:50 +0200

    Btn log out with his logic

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <v-app-bar class="pl-5" color="primary">
       <v-app-bar-title>Farik</v-app-bar-title>
 
-      <v-btn @click="googleAccess">Acceder</v-btn>
+      <v-btn @click="googleAccess" v-if="!userGoogle">Sign in</v-btn>
+      <v-btn @click="googleOut" v-if="userGoogle">log out</v-btn>
     </v-app-bar>
     <v-main app>
       <v-container>
@@ -19,15 +20,29 @@
 import { ref } from "vue";
 import Chat from "./componentes/Chat.vue";
 import FormAdd from "./componentes/FormAdd.vue";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
 import { auth } from "./firebase";
 const googleAccess = async () => {
   try {
-    const provider = new GoogleAuthProvider()
+    const provider = new GoogleAuthProvider();
     const { user } = await signInWithPopup(auth, provider);
     console.log(user);
   } catch (error) {
     console.log(error);
   }
 };
+const userGoogle = ref(false);
+
+const googleOut = async () => {
+  await signOut(auth);
+};
+onAuthStateChanged(auth, (user) => {
+  console.log(user);
+  userGoogle.value = user;
+});
 </script>
